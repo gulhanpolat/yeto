79838b47ae51fc52a370ab81669d9ecdd7f0859c
gulhanpolat <116720491+gulhanpolat@users.noreply.github.com>
2023-05-31 10:44:20 +0300

    Add files via upload

---
+++ manav.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class manav {
+    public static void main(String[] args) {
+        double armut= 2;
+        float elma = 3,67;
+        double domates = 1,11;
+        double muz = 0,95;
+        double patlican =5,00;
+        Scanner input = new Scanner(System.in);
+        System.out.println("armut kaç kilo? :");
+        double kgA = input.nextInt();
+        Scanner input2= new Scanner(System.in);
+        System.out.println("elma kaç kilo? :");
+        double kgE= input2.nextInt();
+        Scanner input3 = new Scanner(System.in);
+        System.out.println("Domates kaç kilo? :");
+        double kgD = input3.nextInt();
+        Scanner input4 = new Scanner(System.in);
+        System.out.println("muz kaç kilo? :");
+        double kgM = input4.nextInt();
+        Scanner input5 = new Scanner(System.in);
+        System.out.println("patlıcan kaç kilo");
+        double kgP = input5.nextInt();
+
+    }
+}
